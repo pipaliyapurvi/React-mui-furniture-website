66192db70d0956f221cba5303b0ceb0660571e11
Maulik-khambhdiya <maulikkhambhdiya444@gmail.com>
2025-10-08 19:54:07 +0530

    funictur-1

--- src/Hovercard.jsx
+++ src/Hovercard.jsx
@@ -10,16 +10,19 @@ function Hovercard() {
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 0.5 }}>
                     <Grid size={3} className="img-container">
                         <img src="	https://wdtteapoy.wpengine.com/wp-content/uploads/2024/05/Brand-Logo-BG-Images-01.jpg" alt="" />
-                        
+                        <Box className="hover-text">Brown Donkey</Box>
                     </Grid>
-                    <Grid size={3}>
+                    <Grid size={3} className="img-container">
                         <img src="https://wdtteapoy.wpengine.com/wp-content/uploads/2024/05/Brand-Logo-BG-Images-02.jpg" alt="" />
+                        <Box className="hover-text">Baletic</Box>
                     </Grid>
-                    <Grid size={3}>
+                    <Grid size={3} className="img-container">
                         <img src="	https://wdtteapoy.wpengine.com/wp-content/uploads/2024/05/Brand-Logo-BG-Images-03.jpg" alt="" />
+                        <Box className="hover-text">Maskan</Box>
                     </Grid>
-                    <Grid size={3}>
+                    <Grid size={3} className="img-container">
                         <img src="	https://wdtteapoy.wpengine.com/wp-content/uploads/2024/05/Brand-Logo-BG-Images-05.jpg" alt="" />
+                        <Box className="hover-text">Saltwater</Box>
                     </Grid>
                 </Grid>
             </Box>
